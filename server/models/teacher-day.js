d2ddc8cbd529f83a6bebfa0e1d28d685873c7969
Oksana Kustova <q100wa@gmail.com>
2022-09-14 22:04:29 -0400

    Added model teacher-day.

---
+++ server/models/teacher-day.js
@@ -0,0 +1,45 @@
+/*
+============================================
+; Model for celebrators' list 
+;===========================================
+*/
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+//const lineItemDocument = require("../schemas/line-item");
+//const celebrator = require ("../schemas/celebrator");
+
+const teacherDaySchema = new Schema(
+  {
+
+    nursingHome: { type: String },
+    region: { type: String },
+    lastName: { type: String },
+    firstName: { type: String },
+    patronymic: { type: String },
+    dateBirthday: { type: Number },
+    monthBirthday: { type: Number },
+    yearBirthday: { type: Number },
+    comment1: { type: String },
+    comment2: { type: String },
+    specialComment: { type: String },
+    linkPhoto: { type: String },
+    plusAmount: { type: Number },
+    /* oldest: { type: Boolean, default: false },
+    category: { type: String }, */
+    noAddress: { type: Boolean },
+    isReleased: { type: Boolean },
+    fullDayBirthday: { type: String },
+    gender: { type: String },
+    celebrator_id: { type: String },
+    fullData: { type: String },
+    holyday: { type: String },  
+    dateHoliday: { type: Number },
+    monthHoliday: { type: Number },
+    absent:  { type: Boolean, default: false },
+
+  },
+  { collection: "teacher_day" }
+);
+
+module.exports = mongoose.model("TeacherDay", teacherDaySchema);
