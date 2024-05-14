55ceb0804556083bc0938c7c4d401314fb769ee5
Oksana Kustova <q100wa@gmail.com>
2024-05-14 14:00:46 -0400

    21-25

--- server/routes/dobroru-api.js
+++ server/routes/dobroru-api.js
@@ -156,8 +156,8 @@ async function createOrder(newOrder, prohibitedId, restrictedHouses) {
     let period;
     if (newOrder.holiday == "Дни рождения июня 2024") {
         period = {
-            "date1": 16,
-            "date2": 20,
+            "date1": 21,
+            "date2": 25,
             "isActive": true,
             "key": 5,
             "maxPlus": 5, //PLUSES
