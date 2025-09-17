41bbd1bd26112d4924826f1425783b3d2b0d92b6
q100va <q100wa@gmail.com>
2025-09-17 10:49:17 -0400

    3+

--- server/routes/orders-api.js
+++ server/routes/orders-api.js
@@ -2492,7 +2492,7 @@ async function createOrder(newOrder, prohibitedId, restrictedHouses) {
       "date2": 20,
       "isActive": true,
       "key": 0,
-      "maxPlus": 2, //PLUSES1
+      "maxPlus": 3, //PLUSES1
       "secondTime": false,
       "scoredPluses": 2
     }
